d818aec2c18af20dee895516a36276b74c267123
ChristianCampos-55 <gamochris@gmail.com>
2021-12-06 21:49:34 -0500

    [ADD]Project folder and task 0

---
+++ 0x02-huffman_coding/heap/heap_create.c
@@ -0,0 +1,26 @@
+#include "heap.h"
+
+/**
+ * heap_create - creates a Heap data structure
+ * @data_cmp: pointer to a comparison functio
+ * Return: new heap or null
+ */
+
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *h = NULL;
+
+	if (!data_cmp)
+		return (NULL);
+
+	h = malloc(sizeof(*h));
+
+	if (!h)
+		return (NULL);
+
+	h->root = NULL;
+	h->size = 0;
+	h->data_cmp = data_cmp;
+
+	return (h);
+}
