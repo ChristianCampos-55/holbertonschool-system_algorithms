4a1265f27c39838adf0e59c83885262ff77f6a17
ChristianCampos-55 <gamochris@gmail.com>
2021-12-21 13:05:32 -0500

    [FIX]Naming convention error and code refactor

---
+++ 0x02-huffman_coding/huffman/huffman.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - function entry point
+ * @ac: arguments counter
+ * @av: arguments vector
+ * Return: SUCCESS OR FAILURE
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 4)
+		return (printf("./huffman <mode> <filename> <out_filename>\n"),
+        EXIT_FAILURE);
+	if (access(av[2], F_OK) == -1)
+		return (printf("No such file: %s\n", av[2]), EXIT_FAILURE);
+	if (access(av[3], F_OK) != -1)
+		return (printf("File already exists: %s\n", av[3]), EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
